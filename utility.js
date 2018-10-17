e135dba60fc1bc3e709e4233d174c71b431f19c9
Alexey Prudnikov <37368224+alexeyprudnikov@users.noreply.github.com>
2018-10-17 09:15:15 +0200

    Create utility.js

---
+++ utility.js
@@ -0,0 +1,41 @@
+/**
+ * methods encrypt and decrypt string for transfer to php via GET
+ * crypto-js is required al modul or path to library .js file
+ *
+ * author: alexeyprudnikov
+ */
+let CryptoJS = require("crypto-js")
+class Utility {
+  /**
+   * encrypt with method AES-256-CBC, returns encrypted string
+   * @param {String} string
+   * @param {String} key
+   * @param {Boolean} raw - forces to output encrypted string as original, not for GET transfer
+   * @returns {String}
+  */
+  static encryptString (text, key, raw) {
+        let iv = CryptoJS.SHA256(key).toString().substring(0,16)
+        iv = CryptoJS.enc.Utf8.parse(iv)
+        let encrypted = CryptoJS.AES.encrypt(text, key, {iv: iv, mode: CryptoJS.mode.CBC, padding: CryptoJS.pad.Pkcs7})
+        let encryptedString = encrypted.toString()
+        if(raw) { return encryptedString }
+        // + durch _ ersetzen wegen GET Übergabe Problem (+ als Leerzeichen vom PHP angenommen und deswegen falsch enkodiert)
+        return encryptedString.replace(/\+/g,'_')
+    }
+    /**
+     * decrypt with method AES-256-CBC, returns null if failed
+     * @param {String} string
+     * @param {String} key
+     * @returns {String|null}
+     */
+    static decryptString (string, key) {
+        string = string.replace(/_/g,'+')
+        let iv = CryptoJS.SHA256(key).toString().substring(0,16)
+        let decrypted = CryptoJS.AES.decrypt(string, key, {iv: iv, mode: CryptoJS.mode.CBC, padding: CryptoJS.pad.Pkcs7})
+        try {
+            return decrypted.toString(CryptoJS.enc.Utf8)
+        } catch(e) {
+            return null
+        }
+    }
+}
